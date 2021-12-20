3b36c59b93a232d91a5ac37625dac6f36b50de70
Sergey <Flargoin@gmail.com>
2021-12-20 12:32:47 +0300

    js add modal & functionality for a modal

--- js/script.js
+++ js/script.js
@@ -91,5 +91,53 @@ const tabs = document.querySelectorAll('.tabheader__item'),
         }
 
     setClock('.timer', deadline);
+
+    /* Modal */
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'), /* Обьявляем в переменные кнопки, само модальное окно, и кнопку для скрытия окна */
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+    
+        modalTrigger.forEach(btn => {                               /* Перебираем кнопки из псевдомассива и вешаем обработчик события */
+          btn.addEventListener('click', openModal);                 /* Для того чтобы запретить скроллл */
+        });
+
+    function openModal() {                                          /* Чтобы не повторять один и тот же код, вывел его в отдельную функцию */
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+      clearInterval(modalTimerId);                                  /* При открытии модального окна таймер ниже сбивается, чтобы не надоедать пользователю */
+    }
+    function closeModal() {                                         /* Чтобы не повторять один и тот же код, вывел его в отдельную функцию */
+      modal.classList.remove('show');
+      modal.classList.add('hide');
+      document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e)=> {                         /* Закрытие при клике за пределами модального окна */
+      if (e.target === modal) {
+        closeModal();
+      }
+    });
+
+    document.addEventListener('keydown', (e) => {                   /* Закрытие при нажатии на клавишу ESC */
+      if(e.code === "Escape" && modal.classList.contains('show')) {
+        closeModal();
+      }
+    });
+
+    const modalTimerId = setTimeout(openModal, 5000);               /* Открытие модального окна через определённый промежуток времени */
+
+    function showModalByScroll() {
+      if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) { 
+      // PageYOffset - сколько px пользователь отлистал сверху по оси Y + clientHeight - высота видимой части >= scrollHeight - полный скролл страницы (если совпадает условие сработает)
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll);    /* Чтобы удалить обработчик события мы должны сделать ссылку на функцию которая исполняла обработку события */
+      }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);           /*  Когда доскроллим страницу до самого низа, откроется модальное окно */
 });
 
