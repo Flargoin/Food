b1acd5afb82a383f86dbd553de3d90426dffcd01
Sergey <flargoin@gmail.com>
2022-11-24 17:29:46 +0300

    add script for timer

--- js/script.js
+++ js/script.js
@@ -1,43 +1,111 @@
 'use strict';
 
 /* Событие которое ждёт прогрузки DOM */
-document.addEventListener("DOMContentLoaded", () => {
 
-    /* Табы */
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+/* Табы */
+document.addEventListener("DOMContentLoaded", () => {
+    const parentTabs = document.querySelector('.tabheader__items'),
+          tab = document.querySelectorAll('.tabheader__item'),
+          tabContent = document.querySelectorAll('.tabcontent');
 
     function hideTabContent() {
-        tabsContent.forEach(item => {
+        tabContent.forEach(item => {
             item.classList.add('hide');
             item.classList.remove('show', 'fade');
         });
 
-        tabs.forEach(item => {
+        tab.forEach(item => {
             item.classList.remove('tabheader__item_active');
         })
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
+        tabContent[i].classList.add('show', 'fade');
+        tabContent[i].classList.remove('hide');
+
+        tab[i].classList.add('tabheader__item_active');
     }
 
-    tabsParent.addEventListener('click', (e) => {
+    parentTabs.addEventListener('click', (e) => {
         const target = e.target;
-
         if(target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((tab, i) => {
-                if(target == tab) {
+            tab.forEach((item, i) => {
+                if(target == item) {
                     hideTabContent();
                     showTabContent(i);
                 }
-            });
+            })
         }
     });
 
     hideTabContent();
     showTabContent();
+
+
+    // Таймер
+    /* 
+    1) Функция которая получает элементы и будет что-то делать с ними
+    2) Функция определяющая разницу времени пользователя с дедлайном
+    3) Функция обновления таймера
+    */
+
+    const deadline = '2023-03-17';
+
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+            if( t <= 0) {
+                days = 0;
+                hours = 0;
+                minutes = 0;
+                seconds = 0
+            } else {
+                days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+                minutes = Math.floor((t / 1000 / 60 ) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+            }
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if(num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
